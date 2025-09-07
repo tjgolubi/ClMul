b4a58080fd4694e40cb198cbd4399ee184058745
Terry Golubiewski <tjgolubi@netins.net>
2025-09-07 10:51:43 -0500

    ClMulBit for loop.

--- ClMulBit.hpp
+++ ClMulBit.hpp
@@ -17,4 +17,18 @@ constexpr uint128_t ClMulBit(std::uint64_t x, std::uint64_t y) noexcept {
   return r;
 } // ClMulBit
 
+#if 0
+/// Multiply two 64-bit unsigned integers using carryless multiplication.
+/// The result is a 128-bit XOR-accumulated product over GF(2).
+constexpr uint128_t ClMulBit(std::uint64_t x, std::uint64_t y) noexcept {
+  auto r = uint128_t{0};
+  auto a = uint128_t{x};
+  for (unsigned i=0; i!=64; ++i) {
+    if ((y>>i) & 1)
+      r ^= (a<<i);
+  }
+  return r;
+} // ClMulBit
+#endif
+
 } // tjg
